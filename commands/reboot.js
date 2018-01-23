7481a5f7c003671f35008f242e158e2e251e5403
maxruahar <maximillionrh@gmail.com>
2018-01-23 05:15:31 +0000

    Added PM2 and reboot command for remote usage

---
+++ commands/reboot.js
@@ -0,0 +1,22 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+	await message.reply("Shutting down.");
+	client.commands.forEach(async cmd => {
+		await client.unloadCommand(cmd);
+	});
+	process.exit(1);
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: ["rb"],
+	permLevel: "Bot Admin",
+	guilds: []
+};
+
+exports.help = {
+	name: "reboot",
+	category: "System",
+	description: "Shuts down the bot. Will attempt to automatically restart.",
+	usage: "reboot"
+};
