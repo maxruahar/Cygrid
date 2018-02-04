b16cf1d35b977c9e406d2d7b9d043955b043d811
maxruahar <maximillionrh@gmail.com>
2018-02-04 21:28:44 +0000

    add big command skeleton. permissions tweak

---
+++ commands/big.js
@@ -0,0 +1,18 @@
+exports.run = (client, message, args, level) => {
+	
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: "User",
+	guilds: []
+};
+
+exports.help = {
+	name: "big",
+	category: "Fun",
+	description: "Big if true.",
+	usage: "big [message]"
+};
