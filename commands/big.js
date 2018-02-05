aaa55a25ea974d19985e6ad6a37fd0947309844e
maxruahar <maximillionrh@gmail.com>
2018-02-05 00:55:07 +0000

    initial big commit

--- commands/big.js
+++ commands/big.js
@@ -1,5 +1,15 @@
 exports.run = (client, message, args, level) => {
-	
+	let msg = args.split(""), end = "";
+	let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+	for (i = 0; i < msg.length; i++) {
+	if (alphabet.includes(msg[i].toLowerCase())) {
+		end += ":regional_indicator_" + msg[i].toLowerCase() + ":";
+	} else if (msg[i] == " ") {
+		end += msg[i] + msg[i] + msg[i];
+	} else end += msg[i];
+	}
+message.channel.send(end);
 };
 
 exports.conf = {
