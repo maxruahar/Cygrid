235f19f0969fd228a6e8888f71c0f5d32823166e
maxruahar <maximillionrh@gmail.com>
2018-04-16 23:39:28 +0000

    remove trigger message after 7500ms

--- commands/poll.js
+++ commands/poll.js
@@ -10,6 +10,7 @@ exports.run = (client, message, args, level) => {
 	let pResponse = "";
 	let pEmbed = {};
 	let i = 0;
+	message.delete(7500);
 
 	pOpts.forEach(o => {
 //		pResponse += `:regional_indicator_${alphabet[i]}: ‣ ${o}\n`;
