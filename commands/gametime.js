85dd2376f25c7b746267f9d55947cd63a9a2d082
maxruahar <maximillionrh@gmail.com>
2018-06-05 19:25:17 +0000

    add comments, unify standard

--- commands/gametime.js
+++ commands/gametime.js
@@ -1,34 +1,41 @@
 exports.run = (client, message, args, level) => {
 
+	//Define new date object with current time
 	const d = new Date();
+
+	//Function for formatting times
 	function timeStamp(now) {
-		// Create a date object with the current time
-		// var now = new Date();
 
-		// Create an array with the current month, day and time
-		const date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+		//Create an array with the current month, day and time
+		const date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
 
-		// Create an array with the current hour, minute and second
-		let time = [ now.getHours(), now.getMinutes() ];
+		//Create an array with the current hour and minute
+		let time = [now.getHours(), now.getMinutes()];
+
+		//Define hour seperately as one above is overwritten later in function
 		let hour = now.getHours();
-		// Determine AM or PM suffix based on the hour
-		const suffix = ( time[0] < 12 ) ? "AM" : "PM";
 
-		// Convert hour from military time
-		time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+		//Determine AM or PM suffix based on the hour
+		const suffix = (time[0] < 12) ? "AM" : "PM";
 
-		// If hour is 0, set it to 12
+		//Convert hour from military time
+		time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
+
+		//If hour is 0, set it to 12
 		time[0] = time[0] || 12;
 
-		// If seconds and minutes are less than 10, add a zero
-		if ( time[1] < 10 ) time[1] = "0" + time[1];
-		if (hour < 10) hour = "0" + hour;
-		// Return the formatted string
-		return hour + ":" + time[1] + " **or** " + time.join(":") + " " + suffix;
+		//If hours or minutes are less than 10, add a zero
+		if (time[1] < 10) time[1] = `0${time[1]}`;
+		if (hour < 10) hour = `0${hour}`;
+
+		//Return the formatted string
+		return `${hour}:${time[1]} **or** ${time.join(":")} ${suffix}`;
 	}
 	if (!args[0]) {
+		//Send current in-game time to channel if no arguments are provided
 		return message.channel.send(`Runescape game time is currently: **${timeStamp(d)}**`);
 	} else if (!isNaN(args[0])) {
+		//Send in-game time after specified number of hours
 		d.setHours(d.getHours() + Number(args[0]));
 		return message.channel.send(`Runescape game time will be **${timeStamp(d)}** in **${args[0]}** hours`);
 	}
