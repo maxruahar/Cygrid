c3289c3f7c134ba128cf42fc19e3d8bcfd68666f
maxruahar <maximillionrh@gmail.com>
2018-06-06 14:56:59 +0000

    add skilling auto bans function

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,8 @@
 module.exports = (client, member) => {
-//	const settings = client.settings.get(member.guild.id);
+	const settings = client.settings.get(member.guild.id);
+	const logChannel = client.channels.get(settings.logChannel);
+	if (member.guild.id == "336818736810164235" && Object.keys(client.skillingBans).includes(member.user.id)) {
+		member.ban(`${member.user.username} was banned automatically upon joining as they are on the auto-ban list.`);
+		logChannel.send(`${member.user.username} was banned automatically upon joining as they are on the auto-ban list.`);
+	}
 }
