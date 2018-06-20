979d3d0be1576e9eb0ab3d38624eedce3764d42a
maxruahar <maximillionrh@gmail.com>
2018-06-20 01:08:15 +0000

    checking content

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ const init = async () => {
 		const thisLevel = client.config.permLevels[i];
 		client.levelCache[thisLevel.name] = thisLevel.level;
 	}
-
 	client.login(client.config.token);
 
 };
