45b5654eca0cc21f80a8144f24e385072997f023
maxruahar <maximillionrh@gmail.com>
2018-09-09 11:58:45 +0000

    empty test for general use

--- commands/test.js
+++ commands/test.js
@@ -1,31 +1,8 @@
 exports.run = (client, message, args, level) => {
-  const { inspect } = require("util");
-  var request = require('request');
+	const { inspect } = require("util");
+	var request = require('request');
 
-/*  var options = {
-    url: 'https://api.twitch.tv/kraken/streams/runescape',
-    headers: {
-      'Client-ID': 'vyxo0jjtz4zpjr3tc6k122qlz8iaxd'
-    }
-  };
 
-  function callback(error, response, body) {
-    if (JSON.parse(body).stream !== null) {
-      message.channel.send("Online!");
-    } else {
-      message.channel.send("Offline.");
-    }
-  }
-
-  request(options, callback);*/
-
-	let x = "false";
-	if (x._called) message.channel.send('not as you thought');
-	x = setTimeout(() => {message.channel.send('test')}, 2000);
-	setTimeout(() => {if (x._called == false) message.channel.send('false')}, 1000);
-	setTimeout(() => {if (x._called == true) message.channel.send('true1')}, 1000);
-	setTimeout(() => {if (x._called == false) message.channel.send('false2')}, 4000);
-	setTimeout(() => {if (x._called == true) message.channel.send('true')}, 4000);
 
 };
 
