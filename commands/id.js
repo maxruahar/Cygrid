41e0222bff9964e82c7937ca59d0da58b722d641
maxruahar <maximillionrh@gmail.com>
2018-09-13 11:04:06 +0000

    initial commit of id command, copied from AHelp

---
+++ commands/id.js
@@ -0,0 +1,39 @@
+exports.run = (client, message, args, level) => {
+
+	const type = args[0];
+	const search = args.slice(1).join(" ");
+
+	const x = message.guild.roles.find('name', args.join(" ")) !== null
+	        ? message.guild.roles.find('name', args.join(" ")).id
+	        : "Invalid role name";
+
+	if (type == "c" && message.guild.channels.find('name', search)) {
+		message.channel.send(`${search}: ${message.guild.channels.find('name', search).id}`);
+	}
+	else if (type == "u" && message.guild.members.find(m => m.user.username == search)) {
+		message.channel.send(`${search}: ${message.guild.members.find(m => m.user.username == search).user.id}`);
+	}
+	else if (type == "r" && message.guild.roles.find('name', search)) {
+		message.channel.send(`${search}: ${message.guild.roles.find('name', search).id}`);
+	}
+	else {
+		message.channel.send("Something went wrong I guess. ¯\_(ツ)_/¯");
+	}
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "Administrator",
+  guilds: [],
+  cooldown: 2500
+};
+
+exports.help = {
+  name: "id",
+  category: "System",
+  description: "Used to collect channel/user/role IDs.",
+  usage: "id <c/u/r> <name>"
+};
