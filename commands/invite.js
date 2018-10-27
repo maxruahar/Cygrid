4e15eb834cf82b50b70c30cf388bb29d1c98be05
Maximillion <maximillionrh@gmail.com>
2018-10-27 03:16:17 +0100

    add lower case searching and correct search term

--- commands/invite.js
+++ commands/invite.js
@@ -3,8 +3,8 @@ exports.run = async (client, message, args, level) => {
   if (!args[0] && message.guild.invite) return message.channel.send(message.guild.invite);
   if (Number(args[0]) && client.guilds.has(args[0])) {
     return message.channel.send(client.guilds.get(args[0]).invite);
-  } else if (client.guilds.map(g => g.name).includes(args[0])) {
-    return message.channel.send(client.guilds.find("name", args[0]).invite);
+  } else if (client.guilds.map(g => g.name.toLowerCase()).includes(args.join(" ").toLowerCase())) {
+    return message.channel.send(client.guilds.find("name", args.join(" ")).invite);
   }
 
 };
