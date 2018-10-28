23e097c2dd50fc917a2a9da569550fe90d047e1c
Maximillion <maximillionrh@gmail.com>
2018-10-28 01:43:57 +0100

    initial commit of command

---
+++ commands/clear.js
@@ -0,0 +1,58 @@
+exports.run = async (client, message, args, level) => {
+
+  //If a user is mentioned, get their user object
+  const user = message.mentions.users.first();
+
+  //Find number of messages to remove by checking if arguments are numbers
+  //in case of a user being specified
+  const num = !!parseInt(args[0]) ? parseInt(args[0]) : parseInt(args[1]);
+
+  //If no number of messages is specified, return error message to channel
+  if (!num) return message.channel.send("Please specify an amount of messages to remove.");
+
+  //Fetch maximum number of messages possible from channel
+  //then filter messages of user if specified
+  message.channel.fetchMessages({limit: 100})
+    .then(messages => {
+
+      //If a user is specified, filter to select only their messages
+      if (user) {
+        messages = messages.filter(m => m.author.id == user.id).array().slice(0, num);
+
+      //Otherwise select messages up to stated amount
+      } else {
+        messages = messages.array().slice(0, num);
+      }
+
+      //Delete the selected messages
+      message.channel.bulkDelete(messages)
+        .then(msgs => {
+
+          //Populate the confirmation message based on whether a user was provided or not
+          const msg = user ? `Removed ${msgs.size} messages from ${user.username}.` : `Removed ${msgs.size} messages.`;
+
+          //Send confirmation message to channel
+          message.channel.send(msg);
+        })
+
+        //Log error to console if there is one
+        .catch(error => console.log(error.stack));
+    });
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["purge"],
+  permLevel: "Administrator",
+  guilds: [],
+  cooldown: 2500
+};
+
+exports.help = {
+  name: "clear",
+  category: "Moderation",
+  description: "Remove messages from a channel.",
+  usage: "clear [user] <number>"
+};
