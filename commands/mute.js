8d799b732ef6da7779525bc43cd99bd8302ddef0
maxruahar <maximillionrh@gmail.com>
2019-03-10 02:20:12 +0000

    disable muting until reworked

--- commands/mute.js
+++ commands/mute.js
@@ -36,7 +36,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 };
 
 exports.conf = {
-	enabled: true,
+	enabled: false,
 	guildOnly: true,
 	aliases: [],
 	permLevel: "Administrator",
--- commands/unmute.js
+++ commands/unmute.js
@@ -41,7 +41,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 };
 
 exports.conf = {
-	enabled: true,
+	enabled: false,
 	guildOnly: true,
 	aliases: [],
 	permLevel: "Administrator",
