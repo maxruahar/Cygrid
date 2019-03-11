aca4b05ce4dbdc6f607b1497e96531d81603e75d
maxruahar <maximillionrh@gmail.com>
2019-03-11 16:43:20 +0000

    add template

--- commands/affiliate.js
+++ commands/affiliate.js
@@ -1,11 +1,13 @@
 exports.run = (client, message, [action, cygID, ...args], level) => {
 
+  const settings = client.settings.get(message.guild.id);
   const db = client.affiliates;
   const mcs = (msg) => message.channel.send(msg);
+  action = action.toLowerCase();
 
   if (!action) return mcs("Please specify an action.");
 
-  if (action == "submit") {
+  if (level >= 4 && ["temp", "template"].includes(action)) {
     const e = {
       "embed": {
         "author": {
@@ -13,12 +15,34 @@ exports.run = (client, message, [action, cygID, ...args], level) => {
           "url": "https://discord.gg/qqducRK",
           "icon_url": "https://i.imgur.com/8sRFoa6.png"
         },
-        "title": "Click here to submit information for ${guild.name}",
-        "url": "<https://docs.google.com/forms/d/e/1FAIpQLSeRUtO6CFH1xfYLDKZK1G4g66Yv0KIpLaKrWdv0jera8IkodA/viewform>",
+        "description": "Description goes here!",
+        "image": {
+          "url": "https://i.imgur.com/NPI0ahN.png"
+        },
         "color": 11842740,
         "footer": {
           "icon_url": "https://i.imgur.com/6c6q2iC.png",
           "text": "Use ${settings.prefix}help affiliate for more commands"
+        }
+      }
+    };
+    mcs(e);
+  } else 
+
+  if (["sub", "submit"].includes(action)) {
+    const e = {
+      "embed": {
+        "author": {
+          "name": "RuneScape Affiliates",
+          "url": "https://discord.gg/qqducRK",
+          "icon_url": "https://i.imgur.com/8sRFoa6.png"
+        },
+        "title": "Click here to submit information for **${message.guild.name}**.",
+        "url": "https://docs.google.com/forms/d/e/1FAIpQLSeRUtO6CFH1xfYLDKZK1G4g66Yv0KIpLaKrWdv0jera8IkodA/viewform",
+        "color": 11842740,
+        "footer": {
+          "icon_url": "https://i.imgur.com/6c6q2iC.png",
+          "text": `Use ${settings.prefix}help affiliate for more commands`
         }
       }
     };
