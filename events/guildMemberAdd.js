bfefcfa066c3334bd08a2a8babc71fc0483374e5
maxruahar <maximillionrh@gmail.com>
2019-04-06 04:21:12 +0100

    add user join warn list

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,7 @@
 module.exports = async (client, member) => {
 	const settings = client.settings.get(member.guild.id);
 	const logChannel = client.channels.get(settings.logChannel);
+	if (["212672527405678592"].includes(member.id)) client.users.get("97928972305707008").send(`${member.user.username} has joined ${member.guild.name}.\n${member.guild.invite}`)
 	if (member.guild.id == "336818736810164235" && Object.keys(client.skillingBans).includes(member.user.id)) {
 		member.ban(`${member.user.username} was banned automatically upon joining as they are on the auto-ban list.`);
 		logChannel.send(`${member.user.username} was banned automatically upon joining as they are on the auto-ban list.`);
@@ -13,9 +14,9 @@ module.exports = async (client, member) => {
 		|| /add pls/gi.test(member.user.username)
 		|| /twitch.tv/gi.test(member.user.username)
 		|| /twitter.com/gi.test(member.user.username)
-		|| /selling/gi.test(member.user.username)
+//		|| /selling/gi.test(member.user.username)
 		|| /\.gg/gi.test(member.user.username)
-		|| /tweet/gi.test(member.user.username)) {
+//		|| /tweet/gi.test(member.user.username)) {
 		member.ban(`${member.user.username} was banned automatically upon joining as their username seems to be advertising other servers/websites.`);
 //		if (member.user.lastMessage.channel.messages.get(member.user.lastMessageID).type == "GUILD_MEMBER_JOIN") {
 //		await client.wait(10000);
