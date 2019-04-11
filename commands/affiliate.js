7c30fec8b7ba98165614c6f53c5943770c9dea69
maxruahar <maximillionrh@gmail.com>
2019-04-11 04:48:21 +0100

    update removal of missing messages and logging

--- commands/affiliate.js
+++ commands/affiliate.js
@@ -386,7 +386,9 @@ exports.run = async (client, message, [action, cygID, ...args], level) => {
         }
         catch (e) {
           errs.push(`${guildID}: **${client.guilds.get(guildID).name}**`);
-          delete client.affMessages.get(cygID)[guildID];
+          const rec = client.affMessages.get(cygID);
+          delete rec[guildID];
+          client.affMessages.set(cygID, rec);
         }
       }
     }
@@ -410,8 +412,10 @@ exports.run = async (client, message, [action, cygID, ...args], level) => {
         }
       }
     };
-    if (errs.length > 1) errs.push(`Any servers where messages could not be found will need to have the **${affEmbed.serverName}** affiliate embed reposted.`);
-    if (errs.length > 2) eUpdate.description += errs.join("\n");
+    if (errs.length > 1) {
+      errs.push(`Any servers where messages could not be found will need to have the **${affEmbed.serverName}** affiliate embed reposted.`);
+      eUpdate.description = `The affiliate embed for **${affEmbed.serverName}** was updated in **${i}/${guilds.length}** servers. ${errs.join("\n")}`;
+    }
     errs.length > 1
       ? mcs(`The affiliate embed for **${affEmbed.serverName}** was updated in **${i}/${guilds.length}** servers. ${errs.join("\n")}`)
       : mcs(`The affiliate embed for **${affEmbed.serverName}** was updated in **${i}/${guilds.length}** servers.`);
