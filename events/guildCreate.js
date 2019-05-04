d6c5b8220cae452a1736b76d9b8dc03a43582c37
maxruahar <maximillionrh@gmail.com>
2019-05-04 03:07:46 +0100

    update guilds adding and removing Cygrid log

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,3 @@
 module.exports = async (client, guild) => {
-  client.guilds.get("433447855127003157").channels.get("574052290755821588").send(`Cygrid has been added to **${guild.id}: ${guild.name}**.`);
+  client.guilds.get("433447855127003157").channels.get("574052290755821588").send(`**${guild.id}:** Cygrid has been added to **${guild.name}**.`);
 };
