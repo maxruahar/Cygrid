8ac640aeda7fac233de7714835446dd07c2a8942
maxruahar <maximillionrh@gmail.com>
2019-06-07 20:44:04 +0100

    remove guild from settings

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,3 +1,4 @@
 module.exports = async (client, guild) => {
   client.guilds.get("433447855127003157").channels.get("574052290755821588").send(`**${guild.id}:** Cygrid has been removed from **${guild.name}**.`);
+  client.settings.delete(guild.id);
 };
