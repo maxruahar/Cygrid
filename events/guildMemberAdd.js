e6eaf15caa24c4653f145d3103b28ad9b5021690
maxruahar <maximillionrh@gmail.com>
2019-06-17 20:49:51 +0100

    allow disabling of banning spam accounts

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,21 +7,23 @@ module.exports = async (client, member) => {
 		member.ban(`${member.user.username} was banned automatically upon joining as they are on the auto-ban list.`);
 		logChannel.send(`${member.user.username} was banned automatically upon joining as they are on the auto-ban list.`);
 	}
-	if (/discord.gg/gi.test(member.user.username)
-		|| /discord.me\/\w+/gi.test(member.user.username)
-		|| /add me/gi.test(member.user.username)
-		|| /pls add/gi.test(member.user.username)
-		|| /add pls/gi.test(member.user.username)
-		|| /twitch.tv/gi.test(member.user.username)
-		|| /twitter.com/gi.test(member.user.username)
-//		|| /selling/gi.test(member.user.username)
-//		|| /\.gg/gi.test(member.user.username)
-//		|| /tweet/gi.test(member.user.username)) 
-		) {
-		member.ban(`${member.user.username} was banned automatically upon joining as their username seems to be advertising other servers/websites.`);
-//		if (member.user.lastMessage.channel.messages.get(member.user.lastMessageID).type == "GUILD_MEMBER_JOIN") {
-//		await client.wait(10000);
-//			member.user.lastMessage.delete();
-//		}
+	if (settings.spamBan) {
+		if (/discord.gg/gi.test(member.user.username)
+			|| /discord.me\/\w+/gi.test(member.user.username)
+			|| /add me/gi.test(member.user.username)
+			|| /pls add/gi.test(member.user.username)
+			|| /add pls/gi.test(member.user.username)
+			|| /twitch.tv/gi.test(member.user.username)
+			|| /twitter.com/gi.test(member.user.username)
+	//		|| /selling/gi.test(member.user.username)
+	//		|| /\.gg/gi.test(member.user.username)
+	//		|| /tweet/gi.test(member.user.username)) 
+			) {
+			member.ban(`${member.user.username} was banned automatically upon joining as their username seems to be advertising other servers/websites.`);
+	//		if (member.user.lastMessage.channel.messages.get(member.user.lastMessageID).type == "GUILD_MEMBER_JOIN") {
+	//		await client.wait(10000);
+	//			member.user.lastMessage.delete();
+	//		}
+		}
 	}
 }
