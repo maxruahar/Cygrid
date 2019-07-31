32f446753311d77808578d50fe40730605614a62
maxruahar <maximillionrh@gmail.com>
2019-07-31 19:35:39 +0100

    initial commit

---
+++ commands/quote.js
@@ -0,0 +1,60 @@
+exports.run = async (client, message, args, level) => {
+	message.delete();
+	let msg, chan, guild, ref, m, c, g;
+	if (/https?:\/\/discordapp.com\/channels\/\d+\/\d+\/\d+/.test(args.join(" "))) {
+	  [g, c, m] = args.join(" ").split("/").splice(4, 3);
+	} else {
+	  [m, c, g] = args.splice(0, 3);
+	}
+
+	if (!m) return;
+	guild = !g
+	  ? message.guild : client.guilds.has(g)
+	    ? client.guilds.get(g) : message.guild;
+
+	chan = guild.channels.has(c)
+	  ? guild.channels.get(c) : message.channel;
+
+	ref = guild.invite ? `[${guild.name}](${guild.invite})` : guild.name;
+
+	try {
+		msg = await chan.fetchMessage(m);
+		if (!msg.content) return;
+		const e = {
+		  "embed": {
+		    "description": `**Message from** ${ref}:\n${msg.content}\n\n**[Jump to message](${msg.url})**`,
+		    "color": msg.member.displayColor,
+		    "timestamp": msg.createdTimestamp,
+		    "footer": {
+		      "icon_url": msg.guild.iconURL,
+		      "text": `User ID: ${msg.author.id}`
+		    },
+		    "author": {
+		      "name": `${msg.member.displayName} | ${msg.author.tag}`,
+		      "icon_url": msg.author.displayAvatarURL
+		    }
+		  }
+		};
+
+		message.channel.send(e);
+	}
+	catch (e) {
+	  message.channel.send("Message could not be found.");
+	}
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["q"],
+  permLevel: "Moderator",
+  guilds: [],
+  cooldown: 2500
+};
+
+exports.help = {
+  name: "quote",
+  category: "Info",
+  description: "This is a test command.",
+  usage: "test\ntest\ntest"
+};
